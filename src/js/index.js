1aecd09b467cceb1e8a01bdc8b77b0cebb812c5f
437873573 <437873573@qq.com>
2018-04-19 18:16:12 +0800

    '自考调整边框，多行文字显示'

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// import {Assemble} from 'assemble'
+// let app=Assemble()
+// $(function () {
+//     let data=[1,2,3,4,5,6];
+//     var contact=app.compile($("#test").html());
+//     $('.test').html(contact(data))
+// })
